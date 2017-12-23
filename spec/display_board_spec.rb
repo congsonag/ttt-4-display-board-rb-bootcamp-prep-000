5b6dbe5f62ee306eb647ad1d91847ff258120170
Stephane <son.trinh.dev@gmail.com>
2017-12-23 02:27:34 +0000

    automatically backed up by learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -156,7 +156,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       expect(rows[0]).to eq(" O | O | O ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" 0 | O | O ")
+      expect(rows[2]).to eq(" O | O | O ")
       expect(rows[3]).to eq("-----------")
       expect(rows[4]).to eq(" O | O | O ")
 
